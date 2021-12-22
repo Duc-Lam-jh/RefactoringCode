3b6b837c3225691818867fbbe166fae73896aee2
Duc-Lam-jh <duc.lam@journeyh.io>
2021-12-22 16:02:11 +0700

    change identifier names according to convention

--- map/map.js
+++ map/map.js
@@ -21,70 +21,62 @@ const Markers = () => {
 	};
 };
 
-function HideElement(id){
-	if (!document.getElementById(id).classList.contains('none')) {
-		document.getElementById(id).classList.add('none');
+function hideElement(id) {
+	if (document.getElementById(id).classList.contains('none')) {
+		return;
 	}
+	document.getElementById(id).classList.add('none');
 }
 
 /* ========= Declarations ========== */
-const markers = Markers(); //an array to store the available markers on the map
-let navigate = false; //a flag to indicate if the navigating function is turned on
-mapboxgl.accessToken =
-	'pk.eyJ1IjoiZHVjbGFtMjI3IiwiYSI6ImNrd3Z6OHhqZDA4a3cyb3M4czltcHAwZXMifQ.5bneNUlldaEBHRv9vr0vNA'; //personal access token
-const map = new mapboxgl.Map({
-	container : 'map',
-	style     : 'mapbox://styles/mapbox/streets-v11',
-	zoom      : 15,
+mapboxgl.accessToken = 'pk.eyJ1IjoiZHVjbGFtMjI3IiwiYSI6ImNrd3Z6OHhqZDA4a3cyb3M4czltcHAwZXMifQ.5bneNUlldaEBHRv9vr0vNA';
+const availableMarkers = Markers();
+const mapNavigationControls = new mapboxgl.NavigationControl();
+let isNavigatingFuncOn = false;
+let debounceTimer;
+const mainMap = new mapboxgl.Map({
+	container: 'map',
+	style: 'mapbox://styles/mapbox/streets-v11',
+	zoom: 15,
 }); //initial declaration of the main map into the div 'map' with zoom level 15
 
-//declare the zoom and rotate controls and add them to the map
-const nav = new mapboxgl.NavigationControl();
-map.addControl(nav);
-
-//debounce timer to serve the searchbox
-let debounceTimer;
-function Debounce (func, time) {
+function debounce(func, time) {
 	debounceTimer = clearTimeout(debounceTimer);
 	debounceTimer = window.setTimeout(func, time);
 }
 
 /* === Initialization of the map === */
-//FUNCTION to set up the map
-function LoadMap () {
-	//get user's current location, if success, call function SuccessLocation, otherwise call ErrorLocation
-	GoToMyLocation();
-
-	//set up search events
-	SetUpSearch();
+function loadMap() {
+	goToMyLocation();
+	setUpSearch();
 }
 
-//FUNCTION: when successfully got user's location, zoom in on it on the map
-function SuccessLocation (position) {
-	FlyToPlace([ position.coords.longitude, position.coords.latitude ]);
-}
-
-//FUNCTION: if cannot get user's location, zoom into the default coords
-function ErrorLocation () {}
-
-//FUNCTION to jump to my current location
-function GoToMyLocation () {
-	navigator.geolocation.getCurrentPosition(SuccessLocation, ErrorLocation, {
-		enableHighAccuracy : true,
+function goToMyLocation() {
+	//if successfully get position, execute successLocation, otherwise errorLocation
+	navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+		enableHighAccuracy: true,
 	});
 }
 
-function SetUpSearch(){
+function successLocation(position) {
+	flyToPlace([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+	flyToPlace([0, 0]);
+}
+
+function setUpSearch() {
 	//add searching functionality to the original searchbox
 	const searchbox = document.getElementById('searchbox');
 	searchbox.addEventListener('keyup', function () {
-		Debounce(() => SearchForPlace(''), 500);
+		debounce(() => searchForPlace(''), 500);
 	});
 
 	//add search functionality to the second searchbox (for navigating)
 	const secondSearchbox = document.getElementById('second-searchbox');
 	secondSearchbox.addEventListener('keyup', function () {
-		Debounce(() => SearchForPlace('second'), 500);
+		debounce(() => searchForPlace('second'), 500);
 	});
 
 	//if the user click outside of the searchboxes, the search results will be hidden
@@ -93,23 +85,32 @@ function SetUpSearch(){
 			e.target != document.getElementById('searchbox') ||
 			e.target != document.getElementById('search-results')
 		) {
-			HideElement('search-results');
+			hideElement('search-results');
 		}
 
 		if (
 			e.target != document.getElementById('second-searchbox') ||
 			e.target != document.getElementById('second-search-results')
 		) {
-			HideElement('second-search-results');
+			hideElement('second-search-results');
 		}
 	});
 }
 
 /* === Search functionality === */
-//FUNCTION to grab the value from searchbox and look for the place
-async function SearchForPlace (id) {
-	id = id.length > 0 ? id + '-' : id;
-	const text = document.getElementById(id + 'searchbox').value;
+/**
+ * Pass in the prefix of a searchbox's id to get the text value 
+ * and call mapbox api to get coordinates
+ * 
+ * @param searchBoxPrefixId
+ * 
+ * Example:
+ * - searchForPlace('') will get text value from the div 'searchbox'
+ * - searchForPlace('second') will get text value from the div 'second-searchbox'
+ */
+async function searchForPlace(searchBoxPrefixId) {
+	searchBoxPrefixId = searchBoxPrefixId.length > 0 ? searchBoxPrefixId + '-' : searchBoxPrefixId;
+	const text = document.getElementById(searchBoxPrefixId + 'searchbox').value;
 
 	if (text.length > 0) {
 		const url =
@@ -119,18 +120,17 @@ async function SearchForPlace (id) {
 			mapboxgl.accessToken;
 
 		const response = await fetch(url, {
-			method : 'GET',
+			method: 'GET',
 		});
 
 		const data = await response.json();
-		LoadResults(data.features, id);
+		loadResults(data.features, searchBoxPrefixId);
 	}
 }
 
-//FUNCTION to show search results
-function LoadResults (array, id) {
+function loadResults(destinationList, searchBoxPrefixId) {
 	//show results box
-	const resultsBox = document.getElementById(id + 'search-results');
+	const resultsBox = document.getElementById(searchBoxPrefixId + 'search-results');
 	resultsBox.innerHTML = '';
 
 	if (resultsBox.classList.contains('none')) {
@@ -138,7 +138,7 @@ function LoadResults (array, id) {
 	}
 
 	//add results into box
-	array.forEach(function (item) {
+	destinationList.forEach(function (item) {
 		const name = document.createElement('div');
 		name.classList.add('name');
 		name.innerHTML = item.text;
@@ -147,53 +147,54 @@ function LoadResults (array, id) {
 		address.classList.add('address');
 		address.innerHTML = item.place_name;
 
-		const searchitem = document.createElement('div');
-		searchitem.classList.add('search-item');
-		searchitem.appendChild(name);
-		searchitem.appendChild(address);
-		searchitem.addEventListener('click', function () {
-			FlyToPlace(item.center);
-			HideElement('search-results');
-			HideElement('second-search-results');
+		const searchItem = document.createElement('div');
+		searchItem.classList.add('search-item');
+		searchItem.appendChild(name);
+		searchItem.appendChild(address);
+		searchItem.addEventListener('click', function () {
+			flyToPlace(item.center);
+			hideElement('search-results');
+			hideElement('second-search-results');
 		});
 
-		resultsBox.appendChild(searchitem);
+		resultsBox.appendChild(searchItem);
 	});
 }
 
-//FUNCTION to zoom in onto a pair of coordinates: center = [longitude, latitude]
-async function FlyToPlace (center) {
+/**
+ * Pass in an array containing the coordinates of the destination
+ * 
+ * @param destination [Longitude, Latitude]
+ */
+async function flyToPlace(destination) {
 	//built-in mapbox method to zoom to a place
-	map.flyTo({
-		center    : center,
-		essential : true,
+	mainMap.flyTo({
+		center: destination,
+		essential: true,
 	});
 
-	//if the navigating flag is false, remove all markers
-	if (!navigate) {
-		markers.removeAll();
+	if (!isNavigatingFuncOn) {
+		availableMarkers.removeAll();
 	}
 	else {
-		//if the navigating flag is on and there are already 2 markers, remove all markers
-		if (markers.length() == 2) {
-			markers.removeAll();
+		if (availableMarkers.length() == 2) {
+			availableMarkers.removeAll();
 		}
 	}
 	//create a new marker and show it on the map
-	const marker = new mapboxgl.Marker().setLngLat(center).addTo(map);
-	markers.append(marker);
+	const marker = new mapboxgl.Marker().setLngLat(destination).addTo(mainMap);
+	availableMarkers.append(marker);
 
-	//if the navigating flag is on and there are 2 markers in total, get the route between the two
-	if (navigate && markers.length() == 2) {
-		//._lngLat is the object that contains the coordinates of the position
-		//convert it into an array to get the [longitude, latitude] format
-		GetRoute(markers.get(0)._lngLat.toArray(), markers.get(1)._lngLat.toArray());
+	if (isNavigatingFuncOn && availableMarkers.length() == 2) {
+		/**_lngLat is the object that contains the coordinates of the position
+		* we convert it into an array to get the [longitude, latitude] format
+		*/
+		getRoute(availableMarkers.get(0)._lngLat.toArray(), availableMarkers.get(1)._lngLat.toArray());
 	}
 }
 
 /* === Navigating functionality === */
-//FUNCTION to get a route between 2 points: start, end = [longitude, latitude]
-async function GetRoute (start, end) {
+async function getRoute(start, end) {
 	//get type: walking, cycling, driving
 	const type = document.getElementById('typeOfDistance').value;
 
@@ -205,7 +206,6 @@ async function GetRoute (start, end) {
 		';' +
 		end +
 		'?geometries=geojson&access_token=pk.eyJ1IjoiZHVjbGFtMjI3IiwiYSI6ImNrd3Z6OHhqZDA4a3cyb3M4czltcHAwZXMifQ.5bneNUlldaEBHRv9vr0vNA';
-	console.log(distanceUrl);
 
 	const response = await fetch(distanceUrl);
 	const data = await response.json();
@@ -218,65 +218,68 @@ async function GetRoute (start, end) {
 	//draw route onto map
 	const route = data.routes[0].geometry.coordinates;
 	const geojson = {
-		type       : 'Feature',
-		properties : {},
-		geometry   : {
-			type        : 'LineString',
-			coordinates : route,
+		type: 'Feature',
+		properties: {},
+		geometry: {
+			type: 'LineString',
+			coordinates: route,
 		},
 	};
 	// if the route already exists on the map, reset it
-	if (map.getSource('route')) {
-		map.getSource('route').setData(geojson);
+	if (mainMap.getSource('route')) {
+		mainMap.getSource('route').setData(geojson);
 	}
 	else {
 		// otherwise, make a new request
-		map.addLayer({
-			id     : 'route',
-			type   : 'line',
-			source : {
-				type : 'geojson',
-				data : geojson,
+		mainMap.addLayer({
+			id: 'route',
+			type: 'line',
+			source: {
+				type: 'geojson',
+				data: geojson,
 			},
-			layout : {
-				'line-join' : 'round',
-				'line-cap'  : 'round',
+			layout: {
+				'line-join': 'round',
+				'line-cap': 'round',
 			},
-			paint  : {
-				'line-color'   : '#667aab',
-				'line-width'   : 5,
-				'line-opacity' : 0.75,
+			paint: {
+				'line-color': '#667aab',
+				'line-width': 5,
+				'line-opacity': 0.75,
 			},
 		});
 	}
 }
 
+
+
 //FUNCTION to toggle the navigation functionality
-function ToggleNavigator () {
+function toggleNavigator() {
 	const secondSearchbox = document.getElementById('secondSearch');
 	const typeDistance = document.getElementById('typeDistance');
 	const distance = document.getElementById('distance');
 
-	//reset the markers
-	markers.removeAll();
+	//reset the availableMarkers
+	availableMarkers.removeAll();
 
 	//if it is off then show its components
 	if (secondSearchbox.classList.contains('none')) {
 		secondSearchbox.classList.remove('none');
 		typeDistance.style.display = 'flex';
-		navigate = true;
+		isNavigatingFuncOn = true;
 	}
 	else {
 		//other wise hide them all
 		secondSearchbox.classList.add('none');
 		typeDistance.style.display = 'none';
 		distance.style.display = 'none';
-		navigate = false;
+		isNavigatingFuncOn = false;
 
-		map.removeLayer('route');
-		map.removeSource('route');
+		mainMap.removeLayer('route');
+		mainMap.removeSource('route');
 	}
 }
 
 /* ========== Execution ========== */
-LoadMap();
+mainMap.addControl(mapNavigationControls);
+loadMap();
